63bb72b8f43d0b69e95e83f41bd768b47bd0d5a6
AmenaynPadre <nsimonyan768@gmail.com>
2026-04-16 17:24:21 +0400

    feat: add user entity with base entity class

--- InstagramClone/Data/AppDbContext.cs
+++ InstagramClone/Data/AppDbContext.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using InstagramClone.Common;
+using InstagramClone.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace InstagramClone.Data;
 
 public class AppDbContext : DbContext
 {
+    public DbSet<User> Users { get; set; }
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
     {
+    }
+    
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker.Entries<BaseEntity>();
+
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Entity.UpdatedAt = DateTime.UtcNow;
+            }
+        }
+
+        return await base.SaveChangesAsync(cancellationToken);
     }
 }
---
+++ InstagramClone/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using InstagramClone.Common;
+
+namespace InstagramClone.Entities;
+
+public class User : BaseEntity
+{
+    [Required]
+    [MaxLength(50)]
+    public string Username { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
+    public string Email { get; set; }
+    
+    [Required]
+    public string PasswordHash { get; set; }
+    
+    public string? Bio { get; set; }
+    
+    public string? ProfilePictureUrl { get; set; }
+}
